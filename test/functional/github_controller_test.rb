35b27a3cf352154fd348201475197d526cb759c2
Flavio Schiavini Abe <flaviosabe@gmail.com>
2013-03-10 23:07:35 -0300

    Test github controller; use WebMock to stub HTTP requests.

--- test/functional/github_controller_test.rb
+++ test/functional/github_controller_test.rb
@@ -4,16 +4,21 @@ class GithubControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_template 'index'
   end
 
   test "should get repos" do
-    get :repos
+    stub_request(:get, 'https://api.github.com/users/fsabe/repos')
+    get :repos, {:user => 'fsabe'}
     assert_response :success
+    assert_template 'repos'
   end
 
   test "should get commits" do
-    get :commits
+    stub_request(:get, 'https://api.github.com/repos/fsabe/rails-demo/commits')
+    get :commits, {:owner => 'fsabe', :repo => 'rails-demo'}
     assert_response :success
+    assert_template 'commits'
   end
 
 end
